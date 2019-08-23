c0e26887e486482e41f2f63916a2d3972ddd83be
dieinimy <dieinimy.domingos@inlog.com.br>
2019-08-23 14:43:06 -0300

    Naive bayes com scikit learn - base crédito - Aula 34

--- naive_bayes_credit_data.py
+++ naive_bayes_credit_data.py
@@ -15,7 +15,6 @@ previsores = base.iloc[:, 1:4].values
 classe = base.iloc[:,4].values
 
 from sklearn.preprocessing import Imputer
-
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(previsores[:, 1:4])
 previsores[:, 1:4] = imputer.transform(previsores[:, 1:4])
@@ -31,3 +30,9 @@ previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = tra
 from sklearn.naive_bayes import GaussianNB
 classificador = GaussianNB()
 classificador.fit(previsores_treinamento, classe_treinamento)
+
+previsoes = classificador.predict(previsores_teste)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+precisao = accuracy_score(classe_teste, previsoes)
+matriz = confusion_matrix(classe_teste, previsoes)
