450a255260704f1c1f8494d9fa53c7e3429144f1
dieinimy <dieinimy.domingos@inlog.com.br>
2019-08-26 12:05:02 -0300

    Incluindo mais um trecho no template_credit_data.py

--- template-credit-data.py
+++ template-credit-data.py
@@ -18,3 +18,12 @@ previsores = scaler.fit_transform(previsores)
 
 from sklearn.model_selection import train_test_split
 previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.25, random_state=0)
+
+# importação da biblioteca
+# criação do classificador
+classificador.fit(previsores_treinamento, classe_treinamento)
+previsoes = classificador.predict(previsores_teste)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+previsao = accuracy_score(classe_teste, previsoes)
+matriz = confusion_matrix(classe_teste, previsoes)
